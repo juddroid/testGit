9fa8edc9b01f231181ce3de355a8240b8286071a
Raccoon <nobllizdc@gmail.com>
2021-07-14 02:49:48 +0900

    test: Fetch repo issue list

---
+++ test-github-api/src/App.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+function App() {
+  const getData = async () => {
+    const response = await fetch(
+      'https://api.github.com/repos/juddroid/ccoonlog/issues',
+      {
+        method: 'GET',
+        'Content-Type': 'application/json',
+        headers: {
+          Authorization: `Basic ${process.env.REACT_APP_TOKEN}`,
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  };
+
+  const handleClickButton = () => getData();
+
+  return (
+    <AppStyle>
+      <Button onClick={handleClickButton}>Click</Button>
+    </AppStyle>
+  );
+}
+
+export default App;
+
+const AppStyle = styled.div`
+  color: #fff;
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  height: 40px;
+`;
